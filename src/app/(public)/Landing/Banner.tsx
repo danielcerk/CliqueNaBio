0c59d15add14f71e72aed177627befb00b2d5ff1
Thyago <thiagogg020676@gmail.com>
2025-02-08 00:38:16 -0300

    style: change title banner

--- src/app/(public)/Landing/Banner.tsx
+++ src/app/(public)/Landing/Banner.tsx
@@ -30,9 +30,9 @@ export default function Banner() {
   }, [scrolling]);
 
   return (
-    <div className={`transition-all duration-500 pt-20  relative ${
+    <div className={`transition-all duration-500 pt-20 min-h-[600px] relative ${
       scrolling > 0 ? "bg-yellow-400" : ""
-    }`}>
+    }`} >
       <div
         className={`py-10 flex flex-col-reverse lg:flex-row gap-5 lg:gap-0 justify-center transition-all duration-500 ${
           scrolling > 0 ? "animate-banner" : "animate-banner-off"
@@ -47,15 +47,15 @@ export default function Banner() {
           data-aos="fade-dissipate"
         />
         <div className="w-full">
-          <div className="text-left max-w-3xl mx-auto px-4 h-full flex flex-col items-center justify-center">
-            <h1 className="text-4xl text-yellow-500 lg:text-6xl uppercase font-extrabold mb-5" data-aos="zoom-in">
+          <div className="text-left max-w-2xl mx-auto px-4 h-full flex flex-col  justify-center">
+            <h1 className="text-4xl text-yellow-500 lg:text-6xl font-extrabold mb-5" data-aos="zoom-in">
               Um Link na bio cheio de memória e experiência
             </h1>
-            <p className="text-2xl italic uppercase max-w-3xl mx-auto font-semibold mb-5" data-aos="zoom-in">
+            <p className="text-xl max-w-3xl mx-auto font-semibold mb-5" data-aos="zoom-in">
               Transforme sua bio em uma vitrine interativa de memórias e conquistas. Personalize, acompanhe o engajamento e expanda sua presença online.
             </p>
             <br />
-            <Link href="/User/Register" className=" bg-gray-900 cursor-pointer text-white w-fit  hover:bg-yellow-400 hover:text-black font-semibold text-xl px-4 py-2 rounded-xl uppercase animate-pulsar transition-all duration-500" data-aos="zoom-in">
+            <Link href="/User/Register" className=" bg-gray-900 cursor-pointer text-white w-fit hover:bg-yellow-400 hover:text-black font-semibold text-xl px-4 py-2 rounded-xl uppercase animate-pulsar transition-all duration-500" data-aos="zoom-in">
               Comece grátis <i className="fa-solid fa-right-long hidden"></i>
             </Link>
           </div>
